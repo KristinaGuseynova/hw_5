c14bbc6dba7f6e55406f407448caa9934cfb2c21
Kristina Guseynova <bittersweet2010@list.ru>
2024-09-22 21:15:59 +0400

    added tests

--- src/test/java/hw5/HW5Test.java
+++ src/test/java/hw5/HW5Test.java
@@ -1,5 +1,6 @@
 package hw5;
 
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -9,7 +10,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class HW5Test {
 
     @Test
-    void openEnterprisePage() {
+    void openEnterprisePageTest() {
         open("https://github.com/");
         $(byText("Solutions")).hover();
         $(byText("Enterprise")).click();
@@ -17,8 +18,11 @@ public class HW5Test {
     }
 
     @Test
-    void dragAndDrop() {
+    void dragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150, 0).release().perform();
+        $("#column-a").shouldHave(text("B"));
 
     }
 
